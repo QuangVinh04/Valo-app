4f4468fbec33d7e50907580ead3848ab9772f462
QuangVinh04 <vinhqlnq004@gmail.com>
2026-06-26 23:43:51 +0700

    Update fix db exist

--- prisma/migrations/20260626160000_drop_indexes_not_in_schema/migration.sql
+++ prisma/migrations/20260626160000_drop_indexes_not_in_schema/migration.sql
@@ -2,13 +2,7 @@
 DROP INDEX IF EXISTS "conversation_attachments_messageId_idx";
 
 -- DropIndex
-DROP INDEX IF EXISTS "conversation_attachments_userId_idx";
-
--- DropIndex
 DROP INDEX IF EXISTS "messages_conversationId_createdAt_idx";
 
 -- DropIndex
 DROP INDEX IF EXISTS "messages_conversationId_idx";
-
--- AlterTable
-ALTER TABLE "conversation_attachments" ALTER COLUMN "id" DROP DEFAULT;
